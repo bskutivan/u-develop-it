51602e1db1cf0fdd92d32f97a7d81f56df8e7d21
BrandonKutivan <bskutivan@gmail.com>
2020-05-27 17:06:41 -0700

    added GET, DELETE, and POST routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 
+DROP TABLE IF EXISTS candidates;
+
 CREATE TABLE candidates (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
